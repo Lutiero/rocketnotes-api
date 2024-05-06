f5b7eeaffa011a12664a85be23d4166be7b423b6
Lutiero <3720@ucdb.br>
2024-05-06 07:54:25 -0400

    feat: Add user creation functionality to users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,9 +2,10 @@ const { Router } = require("express");
 
 const userRoutes = Router();
 
-userRoutes.post("/", (req, res) => {
-  const { name, email } = req.body;
-  res.json({ name, email });
-});
+const UserController = require("../controllers/usersControllers");
+
+const userController = new UserController();
+
+userRoutes.post("/", userController.create);
 
 module.exports = userRoutes;
