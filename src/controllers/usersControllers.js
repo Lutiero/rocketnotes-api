6de3740a4c722fd0f6df685c4a00934382b5ee9c
Lutiero <3720@ucdb.br>
2024-05-06 09:16:14 -0400

    chore: Update npm dependencies and add error handling for user creation

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,8 +1,15 @@
+const AppError = require("../utils/AppError")
+
 class UsersControllers {
   create(req, res) {
-    const { name, email, password } = req.body;
-    res.json({ name, email, password });
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+      throw new AppError("Missing parameters", 400)
+    }
+
+    res.status(201).json({ name, email, password })
   }
 }
 
-module.exports = UsersControllers;
+module.exports = UsersControllers
--- src/server.js
+++ src/server.js
@@ -1,12 +1,27 @@
-const express = require("express");
-const routes = require("./routes");
+require("express-async-errors")
+const express = require("express")
+const routes = require("./routes")
+const AppError = require("./utils/AppError")
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-app.use(routes);
+app.use(routes)
 
-const PORT = 3000;
+app.use((error, req, response, nex) => {
+  if (error instanceof AppError) {
+    return response
+      .status(error.statusCode)
+      .json({ error: "error", message: error.message })
+  }
+  console.log(error)
+
+  return response
+    .status(500)
+    .json({ error: "error", message: "Internal server error" })
+})
+
+const PORT = 3000
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
