a3e6517c8d195df922df71f3c9b883b6b9ece151
Lutiero <3720@ucdb.br>
2024-05-08 10:47:21 -0400

    chore: Update npm dependencies and add jsonwebtoken library

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,8 @@
 const knex = require("../database/knex/")
 const AppError = require("../utils/AppError")
 const { compare } = require("bcryptjs")
+const authConfig = require("../configs/auth")
+const { sign } = require("jsonwebtoken")
 
 class SessionsController {
   async create(request, response) {
@@ -13,7 +15,14 @@ class SessionsController {
 
     if (!passwordMatch) throw new AppError("Email ou senha não incorreta", 401)
 
-    return response.status(200).json(passwordMatch)
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn,
+    })
+
+    return response.status(200).json({ user, token })
   }
 }
 
